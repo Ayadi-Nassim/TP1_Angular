49c9f6e05bef1834d7197e6da096a23a932dd77d
khalil207 <khalil.kdous@insat.ucat.tn>
2024-12-03 17:59:47 +0100

    modified app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,41 +1,45 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Route } from "@angular/router";
-import { TodoComponent } from "./todo/todo/todo.component";
-import { MiniWordComponent } from "./directives/mini-word/mini-word.component";
-import { ColorComponent } from "./components/color/color.component";
-import { FrontComponent } from "./templates/front/front.component";
-import { AdminComponent } from "./templates/admin/admin.component";
-import { LoginComponent } from "./auth/login/login.component";
-import { NF404Component } from "./components/nf404/nf404.component";
-import { AuthGuard } from "./auth/guards/auth.guard";
-import { AddCvComponent } from "./cv/add-cv/add-cv.component";
-import { CvComponent } from "./cv/cv/cv.component";
-import { DetailsCvComponent } from "./cv/details-cv/details-cv.component";
-import { RhComponent } from "./optimizationPattern/rh/rh.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Route } from '@angular/router';
 
 const routes: Route[] = [
-  { path: "login", component: LoginComponent },
-  { path: "rh", component: RhComponent },
   {
-    path: "cv",
-    component: CvComponent,
-  },
-  { path: "cv/add", component: AddCvComponent, canActivate: [AuthGuard] },
-  { path: "cv/:id", component: DetailsCvComponent },
-  {
-    path: "",
-    component: FrontComponent,
-    children: [
-      { path: "todo", component: TodoComponent },
-      { path: "word", component: MiniWordComponent },
-    ],
+    path: 'login',
+    loadComponent: () =>
+      import('./auth/login/login.component').then((m) => m.LoginComponent),
   },
   {
-    path: "admin",
-    component: AdminComponent,
-    children: [{ path: "color", component: ColorComponent }],
+    path: 'rh',
+    loadComponent: () =>
+      import('./optimizationPattern/rh/rh.component').then(
+        (m) => m.RhComponent
+      ),
   },
-  { path: "**", component: NF404Component },
+
+  {
+    path: 'cv',
+    loadChildren: () => import('./cv/cv.module').then((m) => m.CvModule),
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./templates/front/front.module').then(
+        (m) => m.FrontRoutingModule
+      ),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./templates/admin/admin.module').then(
+        (m) => m.AdminRoutingModule
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./components/nf404/nf404.component').then(
+        (m) => m.NF404Component
+      ),
+  },
 ];
 
 @NgModule({

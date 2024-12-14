16717645fb1776ee319755a393937246bfbdc715
khalil207 <khalil.kdous@insat.ucat.tn>
2024-12-14 10:33:41 +0100

    some changes

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -17,29 +17,29 @@ import { Settings } from './dto/product-settings.dto';
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
-  products$!: Observable<Product[]>; // Observable for products
-  private productSettings = new BehaviorSubject<Settings>({ limit: 12, skip: 0 }); // BehaviorSubject to manage settings
+  products$!: Observable<Product[]>; 
+  private productSettings = new BehaviorSubject<Settings>({ limit: 12, skip: 0 }); 
 
   constructor(private productService: ProductService) {
-    // Create the stream for products
+
     this.products$ = this.productSettings.pipe(
-      // Fetch products based on the current settings
+
       concatMap((settings) =>
         this.productService.getProducts(settings).pipe(
           map((response) => response.products)
         )
       ),
-      // Accumulate all fetched products into a single array
+
       scan<Product[], Product[]>(
         (allProducts, newProducts) => [...allProducts, ...newProducts],
-        [] // Start with an empty array
+        [] 
       ),
-      // Stop fetching if no products are left
+
       takeWhile((allProducts) => allProducts.length % 12 === 0, true)
     );
   }
 
-  // Method to load more products
+
   loadMore(): void {
     const currentSettings = this.productSettings.value;
     this.productSettings.next({
